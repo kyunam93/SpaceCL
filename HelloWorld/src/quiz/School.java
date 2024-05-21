6b2a7c1ab506b8f122e9702d3ce44a7e0b1cc939
임규남 <lkn1027@gmail.com>
2024-05-22 00:03:27 +0900

    순위 매기기

--- HelloWorld/src/quiz/School.java
+++ HelloWorld/src/quiz/School.java
@@ -50,45 +50,23 @@ public class School {
 
 	// 학생들의 순위를 구하는 함수
 	public void setRankOfStudents() {
-		ranking = new double[studentList.size()]; // 학생의 인원수 만큼 배열 생성
-
-		// 랭킹 배열에 학생들의 평균점수 초기화
-		for (int i = 0; i < ranking.length; i++) {
-			ranking[i] = studentList.get(i).avg;
-		}
-		for (int i = 0; i < ranking.length; i++) {
-			System.out.print("data[" + i + "] = " + ranking[i] + "\t");
-		}
-		System.out.println();
-
-		int dLen = ranking.length; //7
-		
-		//length-1 의 의미는 마지막 pivot 은 오른쪽으로 검사할
-		//대상이 없음으로 for문을 돌지않게 하기 위한 조치이다.
-		for(int i=1; i<dLen; i++) {  
-			
-			//pivot 값으로 오른쪽으로 계속 비교해주는 for 문이다. 
-			for(int j=0; j < (dLen-i); j++) {
-				
-				//왼쪽에 값이 오른쪽 값보다 크냐?
-				if( ranking[j] > ranking[j+1] ) {
-					//왼쪽값이 작아야된다. 작은순대로 정렬할것이기 때문에
-					//swap 알고리즘 - 현재 왼쪽값을 임시 변수 k 에 저장해둔다.
-					double k = ranking[j];  
-					ranking[j] = ranking[j+1]; //왼쪽자리에 오른쪽에 값을 저장한다.
-					ranking[j+1] = k;		 //오른쪽자리에 왼쪽값을 저장한다.
-				}
-				
-			}
-			
-		}//end for
+        for(int i = 0; i < studentList.size(); i++) {
+        	int cnt = 1;
+        	
+        	for(int j = 0; j < studentList.size(); j++) {
+        		if(studentList.get(j).avg > studentList.get(i).avg) {
+        			cnt += 1;
+        		}//end if
+        	}//end for
+        	studentList.get(i).rank = cnt;
+        }//end for
 
 	}
 
-	// 학생들의 순위를 전부 보여주는 함수\
+	// 학생들의 순위를 전부 보여주는 함수
 	public void showRanking() {
-		for (int i = 0; i < ranking.length; i++) {
-			System.out.println(ranking[i]);
+		for (int i = 0; i < studentList.size(); i++) {
+			System.out.println("학생 "+studentList.get(i).studentName+"의 평균은 "+studentList.get(i).avg+"이므로 등수는 "+studentList.get(i).rank+"입니다");
 		}
 	}
 
