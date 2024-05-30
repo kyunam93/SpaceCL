8dc0ae4e393a75803df385a444037d9d716f1b4b
임규남 <lkn1027@gmail.com>
2024-05-30 13:11:11 +0900

    수업내용_업데이트_람다식&예외처리&스트림

---
+++ HelloWorld/src/chapter14/IDFormatException.java
@@ -0,0 +1,9 @@
+package chapter14;
+
+public class IDFormatException extends Exception {
+
+	public IDFormatException(String msg) {
+		super(msg);
+	} // end consturctor
+
+} // end class
---
+++ HelloWorld/src/chapter14/SystemInTest1.java
@@ -0,0 +1,28 @@
+package chapter14;
+
+import java.io.IOException;
+
+public class SystemInTest1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("알파벳 하나 쓰고 엔터 누르세요");
+
+		int i;
+
+		try {
+
+			// read() 메서드로 한 바이트 읽음
+			i = System.in.read();
+			System.out.println(i);
+			// 문자로 변환하여 출력
+			System.out.println((char) i);
+		} // end try
+
+		catch (IOException e) {
+			e.printStackTrace();
+		} // end catch
+
+	} // end main
+
+} // end class
